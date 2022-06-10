11f1d1b6b1f4749ba5205be9bac68b548547663e
unknown <christian.flores@pcsww.com>
2022-06-10 15:13:25 +0800

    fix chev buttons

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -18,7 +18,7 @@ const Card = ({hasLeftSlide=false,hasRightSlide=false,img,avatar,review,info}) =
         }
 
         {
-            hasLeftSlide && <div className='w-[26px] h-[26px] absolute right-4 rounded-full shadow-chev flex items-center justify-center'>
+            hasLeftSlide && <div className='w-[26px] h-[26px] absolute left-4 rounded-full shadow-chev flex items-center justify-center'>
            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
                 </svg>
